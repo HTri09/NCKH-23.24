182651b0e49634b2beec02cdd9e27a8a9d2ff26a
luhtri <luhtri2109@gmail.com>
2024-03-08 14:58:56 +0700

    refactor: delete authentication

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const passport = require('passport')
-const session = require('express-session')
 const morgan = require('morgan')
 require('dotenv').config()
 
@@ -8,25 +6,12 @@ const route = require('./src/routes/index')
 const connect = require('./src/config/index')
 
 const app = express()
-const PORT = 3000
-
-require('./src/util/auth.js')
+const PORT = process.env.PORT
 
 
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-app.use(session({
-    secret: 'cats',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        secure: false       
-    }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
 
 // Connect to database
 connect()
@@ -37,36 +22,6 @@ app.use(morgan('tiny'))
 
 
 // Routes init
-const { isLoggedIn } = require('./src/middleware/auth.middleware.js')
-
-app.get('/', (req, res) => {
-    res.send('<a href="/auth/google">Authenticate with Google</a>')
-})
-
-app.get('/auth/google',
-    passport.authenticate('google', { scope: ['email', 'profile'] }
-    ))
-
-app.get('/google/callback',
-    passport.authenticate('google', {
-        successRedirect: '/protected',
-        failureRedirect: '/auth/google/failure'
-    })
-)
-
-app.get('/protected', isLoggedIn, (req, res) => {
-    res.send(`Hello ${req.user.displayName}`)
-})
-
-app.get('/logout', async (req, res) => {
-    req.session.destroy()
-    res.send('Goodbye!') 
-})
-
-app.get('/auth/google/failure', (req, res) => {
-    res.send('Failed to authenticate..')
-})
-
 route(app)
 
 

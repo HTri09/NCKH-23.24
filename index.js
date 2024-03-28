835815d80ec52b8877c6e7142e735b08f28f9f92
luhtri <luhtri2109@gmail.com>
2024-03-29 04:56:40 +0700

    feat: error logger and request logger

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-    const express = require('express')
-    const morgan = require('morgan')
-    require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+require('dotenv').config()
 
-    const route = require('./src/routes/index')
-    const connect = require('./src/config/index')
-    const { logRequest } = require('./src/middleware/logRequest.js')
+const route = require('./src/routes/index')
+const connect = require('./src/config/index')
+const { requestLogger, errorLogger } = require('./src/middleware/index.js')
 
-    const app = express()
-    const PORT = process.env.PORT
-
-
-    // Middleware
-    app.use(express.json())
-    app.use(express.urlencoded({ extended: true }))
-    app.use(logRequest)
-
-    // Connect to database
-    connect()
+const app = express()
+const PORT = process.env.PORT
 
 
-    // HTTP Logger
-    app.use(morgan('dev'))
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(requestLogger)
+app.use(errorLogger)
+
+// Connect to database
+connect()
 
 
-    // Routes init
-    route(app)
+// HTTP Logger
+app.use(morgan('dev'))
 
 
-    app.listen(PORT, async () => {
-        console.log(`Listening at http://localhost:${PORT}`)
-    })
+// Routes init
+route(app)
+
+
+app.listen(PORT, async () => {
+    console.log(`Listening at http://localhost:${PORT}`)
+})

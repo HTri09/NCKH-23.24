f33fb6d1dbe46edb82852c87cdba10caa63f14d2
luhtri <luhtri2109@gmail.com>
2024-04-05 03:34:53 +0700

    feat: image upload to Firebase

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,9 @@
 const errorLogger = require('./error.logger')
 const requestLogger = require('./request.logger')
+const multer = require('./multer')
 
 module.exports = {
     errorLogger,
-    requestLogger
+    requestLogger,
+    multer    
 }

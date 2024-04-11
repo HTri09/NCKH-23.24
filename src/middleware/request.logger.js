5de24288a36f11aa0675fb9e847576adff81b232
luhtri <luhtri2109@gmail.com>
2024-04-11 17:14:01 +0700

    feat: automatic rotate server logger

--- src/middleware/request.logger.js
+++ src/middleware/request.logger.js
@@ -1,19 +1,29 @@
 const fs = require('fs')
 const path = require('path')
 const winston = require('winston')
+const DailyRoatateFile = require('winston-daily-rotate-file')
 
 const logger = winston.createLogger({
-    format: winston.format.simple(),
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.simple(),
+        winston.format.colorize(),
+    ),
     transports: [
-        new winston.transports.File({ filename: path.join('logs', 'server.log') })
+        new DailyRoatateFile({
+            filename: 'logs/%DATE%.server.log',
+            datePattern: 'YYYY-MM-DD',
+            maxSize: '20m',
+            maxFiles: '1d'
+        })
     ]
-});
+})
 
-// if (process.env.NODE_ENV !== 'production') {
-//     logger.add(new winston.transports.Console({
-//         format: winston.format.simple(),
-//     }));
-// }
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }))
+}
 
 
 // TODO: rotate log file after 24h

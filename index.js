ce56fba71fc6ff19122bd70f22e8ad1a02bd0e58
luhtri <luhtri2109@gmail.com>
2024-04-12 03:50:57 +0700

    move image to assets

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.clear()
 const express = require('express')
 const morgan = require('morgan')
 require('dotenv').config()
@@ -7,7 +8,7 @@ const { connect } = require('./src/config/index')
 const { requestLogger, errorLogger } = require('./src/middleware/index.js')
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 
 // Middleware
@@ -25,41 +26,8 @@ app.use(morgan('dev'))
 
 
 // Routes init
-// route(app)
+route(app)
 
-
-const multer = require('multer')
-const path = require('path')
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-});
-
-const upload = multer({ storage: storage });
-const send = multer({
-    
-})
-
-// Upload endpoint
-app.post('/upload', upload.single('imageFile'), (req, res) => {
-    try {
-        const filePath = path.join(__dirname, req.file.originalname);
-        console.log(filePath)
-        // Process and store the image as needed
-        console.log('Image uploaded:', filePath);
-        res.json({ message: 'Image uploaded successfully' });
-    } catch (error) {
-        console.error('Error uploading image:', error);
-        res.status(500).json({ error: 'Failed to upload image' });
-    }
-});
-
-app.post('/send', )
 
 app.listen(PORT, async () => {
     console.log(`Listening at http://localhost:${PORT}`)
